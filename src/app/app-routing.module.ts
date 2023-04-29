a2bf2ef619a94f9c9a83392208824165a9b6e835
miguelro911 <miguel.romo.cac@gmail.com>
2023-04-29 12:17:30 -0500

    Ahora si se subio xd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './componentes/main/main.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ListaComponent } from './componentes/lista/lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'main', component:  MainComponent},
+  {path:'registro', component: RegistroComponent},
+  {path: 'lista', component: ListaComponent},
+  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: '**', component: MainComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { MainComponent } from './componentes/main/main.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ListaComponent } from './componentes/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    RegistroComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
